4e201226c9f2d63a024844c267cda73e074cf854
gilbox <birmangil@gmail.com>
2015-07-03 15:45:11 -0500

    update global build

--- build/global/elegant-react.js
+++ build/global/elegant-react.js
@@ -7,7 +7,7 @@
 		exports["ElegantReact"] = factory(require("react"));
 	else
 		root["ElegantReact"] = factory(root["React"]);
-})(this, function(__WEBPACK_EXTERNAL_MODULE_1__) {
+})(this, function(__WEBPACK_EXTERNAL_MODULE_4__) {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -56,26 +56,20 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	var React = __webpack_require__(1);
-	var elegant = __webpack_require__(2);
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+	exports['default'] = setup;
 
-	module.exports = elegant(React);
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
+	var _createElegantDecorator = __webpack_require__(1);
 
-	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+	var _createElegantDecorator2 = _interopRequireDefault(_createElegantDecorator);
 
-/***/ },
-/* 2 */
-/***/ function(module, exports, __webpack_require__) {
+	var _react = __webpack_require__(4);
 
-	'use strict';
-
-	var componentLib = __webpack_require__(3);
-	var StaticsMixin = __webpack_require__(5);
-	var ComponentRenderMixin = __webpack_require__(4);
+	var _react2 = _interopRequireDefault(_react);
 
 	function subedit(edit) {
 	  for (var _len = arguments.length, path = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
@@ -89,148 +83,124 @@ return /******/ (function(modules) { // webpackBootstrap
 	  };
 	}
 
-	var elegant = function elegant(React) {
-	  var component = componentLib(React);
-
-	  var lib = {
+	function setup(debug) {
+	  return {
 	    subedit: subedit,
-	    StaticsMixin: StaticsMixin,
-	    ComponentRenderMixin: ComponentRenderMixin,
-	    component: component.component
+	    sub: subedit,
+	    elegant: (0, _createElegantDecorator2['default'])(_react2['default'], debug)
 	  };
+	}
 
-	  var elegantReact = function elegantReact(options) {
-	    if (options && options.hasOwnProperty('debug')) {
-	      component.enableDebugging(options.debug);
-	    }
-
-	    return lib;
-	  };
-
-	  for (var k in lib) {
-	    elegantReact[k] = lib[k];
-	  }
-
-	  return elegantReact;
-	};
-
-	module.exports = elegant;
+	setup.elegant = (0, _createElegantDecorator2['default'])(_react2['default']);
+	setup.subedit = subedit;
+	setup.sub = subedit;
+	module.exports = exports['default'];
 
 /***/ },
-/* 3 */
+/* 1 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var ComponentRenderMixin = __webpack_require__(4);
-	var StaticsMixin = __webpack_require__(5);
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
 
-	var debug = false;
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-	module.exports = function (React) {
-	  return {
-	    enableDebugging: function enableDebugging(enable) {
-	      return debug = !!enable;
-	    },
+	var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
 
-	    // @param additionalMixins? {Array|Object}
-	    // @param renderFn {Function}
-	    // @returns Component
-	    component: function component(additionalMixins, renderFn) {
-	      renderFn = renderFn || additionalMixins;
-	      additionalMixins = additionalMixins instanceof Function ? [] : [].concat(additionalMixins);
+	exports['default'] = createElegantDecorator;
 
-	      var mixins = [ComponentRenderMixin, StaticsMixin].concat(additionalMixins);
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
 
-	      var render = function render() {
-	        if (debug) console.log('render <' + renderFn.name + ' />');
-	        return renderFn.call(this, this.props, this.props.statics, this.props.children);
-	      };
+	function _inherits(subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
 
-	      var displayName = renderFn.name;
+	var _statics = __webpack_require__(2);
 
-	      return React.createClass({ displayName: displayName, mixins: mixins, render: render });
-	    }
+	var _shallowEqualProps = __webpack_require__(3);
+
+	var getDisplayName = function getDisplayName(Component) {
+	  return Component.displayName || Component.name || 'Component';
+	};
+
+	function createElegantDecorator(React, debug) {
+	  var Component = React.Component;
+
+	  var decorator = function decorator(_ref) {
+	    var statics = _ref.statics;
+
+	    var staticsMap = (0, _shallowEqualProps.createStaticsMap)(statics);
+
+	    return function (DecoratedComponent) {
+	      return (function (_Component) {
+	        function ElegantDecorator() {
+	          _classCallCheck(this, ElegantDecorator);
+
+	          _get(Object.getPrototypeOf(ElegantDecorator.prototype), 'constructor', this).apply(this, arguments);
+	        }
+
+	        _inherits(ElegantDecorator, _Component);
+
+	        _createClass(ElegantDecorator, [{
+	          key: 'shouldComponentUpdate',
+	          value: function shouldComponentUpdate(nextProps, nextState) {
+	            return !(0, _shallowEqualProps.shallowEqualProps)(this.props, nextProps, staticsMap);
+	          }
+	        }, {
+	          key: 'componentWillReceiveProps',
+	          value: function componentWillReceiveProps(newProps) {
+	            (0, _statics.updateStatics)(statics, this.wrappedProps, newProps);
+	          }
+	        }, {
+	          key: 'render',
+	          value: function render() {
+	            if (debug) console.log('render <' + ElegantDecorator.displayName + '>');
+
+	            this.wrappedProps = (0, _statics.wrapStatics)(statics, this.props);
+
+	            return React.createElement(DecoratedComponent, this.wrappedProps);
+
+	            // could be optimized with createElement
+	            // return React.createElement(DecoratedComponent, this.wrappedProps, this.wrappedProps.children);
+	          }
+	        }], [{
+	          key: 'displayName',
+	          value: 'Elegant(' + getDisplayName(DecoratedComponent) + ')',
+	          enumerable: true
+	        }, {
+	          key: 'DecoratedComponent',
+	          value: DecoratedComponent,
+	          enumerable: true
+	        }]);
+
+	        return ElegantDecorator;
+	      })(Component);
+	    };
 	  };
-	};
 
-/***/ },
-/* 4 */
-/***/ function(module, exports) {
-
-	// ComponentRenderMixin is a slightly modified version of PureRenderMixin
-
-	'use strict';
-
-	function shallowEqualProps(objA, objB) {
-	  if (objA === objB) {
-	    return true;
-	  }
-
-	  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-	    return false;
-	  }
-
-	  var keysA = Object.keys(objA);
-	  var keysB = Object.keys(objB);
-
-	  if (keysA.length !== keysB.length) {
-	    return false;
-	  }
-
-	  // Test for A's keys different from B.
-	  var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
-	  for (var i = 0; i < keysA.length; i++) {
-	    if (keysA[i] === 'statics') continue;
-	    if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
-	      return false;
-	    }
-	  }
-
-	  return true;
+	  return function (optionsOrComponent) {
+	    return optionsOrComponent.prototype instanceof Component ? decorator({})(optionsOrComponent) : decorator(optionsOrComponent);
+	  }; // Component : options
 	}
 
-	function shallowEqual(objA, objB) {
-	  if (objA === objB) {
-	    return true;
-	  }
-
-	  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
-	    return false;
-	  }
-
-	  var keysA = Object.keys(objA);
-	  var keysB = Object.keys(objB);
-
-	  if (keysA.length !== keysB.length) {
-	    return false;
-	  }
-
-	  // Test for A's keys different from B.
-	  var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
-	  for (var i = 0; i < keysA.length; i++) {
-	    if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
-	      return false;
-	    }
-	  }
-
-	  return true;
-	}
-
-	var ComponentRenderMixin = {
-	  shouldComponentUpdate: function shouldComponentUpdate(nextProps, nextState) {
-	    return !shallowEqualProps(this.props, nextProps) || !shallowEqual(this.state, nextState);
-	  }
-	};
-
-	module.exports = ComponentRenderMixin;
+	module.exports = exports['default'];
 
 /***/ },
-/* 5 */
+/* 2 */
 /***/ function(module, exports) {
 
 	'use strict';
 
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+	exports.isDelegate = isDelegate;
+	exports.wrapStatics = wrapStatics;
+	exports.updateStatics = updateStatics;
 	function delegate(delegee) {
 	  var delegate = function delegate() {
 	    return delegate.delegee.apply(this, arguments);
@@ -240,54 +210,102 @@ return /******/ (function(modules) { // webpackBootstrap
 	  return delegate;
 	}
 
-	function wrapWithDelegate(key) {
-	  var statics = this;
-	  var delegee = statics[key];
-	  if (typeof delegee === 'function') {
-	    statics[key] = isDelegate(delegee) ? delegee : delegate(delegee);
-	  }
-	}
-
 	function isDelegate(value) {
 	  return value && value.isDelegate;
 	}
 
-	function componentWillMount() {
-	  var statics = this.props.statics;
-	  if (statics && typeof statics === 'object') {
-	    Object.keys(statics).forEach(wrapWithDelegate, statics);
+	// perform on render
+
+	function wrapStatics(statics, props) {
+	  var transformedProps = _extends({}, props);
+	  if (statics) {
+	    statics.forEach(function (key) {
+	      var delegee = props[key];
+	      if (typeof delegee === 'function') {
+	        transformedProps[key] = isDelegate(delegee) ? delegee : delegate(delegee);
+	      }
+	    });
 	  }
+	  return transformedProps;
 	}
 
-	function componentWillReceiveProps(newProps) {
-	  var currentProps = this.props;
-	  var currentStatics = currentProps.statics;
-	  var newStatics = newProps.statics;
-	  var haveChangedStatics = newStatics !== currentStatics && newStatics && typeof newStatics === 'object';
+	// perform when recieving props
 
-	  if (haveChangedStatics) {
-	    Object.keys(newStatics).forEach(function (key) {
-	      var newMember = newStatics[key];
-	      if (typeof newMember == 'function') {
-	        var currentMember = currentStatics && currentStatics[key];
+	function updateStatics(statics, wrappedProps, newProps) {
+	  if (statics && newProps && wrappedProps) {
+	    statics.forEach(function (key) {
+	      var newMember = newProps[key];
+
+	      if (typeof newMember === 'function') {
+	        var currentMember = wrappedProps[key];
 	        if (isDelegate(currentMember)) {
 	          var delegee = isDelegate(newMember) ? newMember.delegee : newMember;
 	          currentMember.delegee = delegee;
-	          newStatics[key] = currentMember;
-	        } else {
-	          newStatics[key] = delegate(newMember);
 	        }
 	      }
 	    });
 	  }
 	}
 
-	var StaticsMixin = {
-	  componentWillMount: componentWillMount,
-	  componentWillReceiveProps: componentWillReceiveProps
-	};
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
 
-	module.exports = StaticsMixin;
+	// convert statics array to map object
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+	exports.createStaticsMap = createStaticsMap;
+	exports.shallowEqualProps = shallowEqualProps;
+
+	function createStaticsMap(statics) {
+	  if (!statics) return {};
+	  var map = {};
+	  statics.forEach(function (key) {
+	    return map[key] = true;
+	  });
+	  return map;
+	}
+
+	;
+
+	// modified version of shallowEqualProps fn from PureRenderMixin
+
+	function shallowEqualProps(objA, objB, staticsMap) {
+	  if (objA === objB) {
+	    return true;
+	  }
+
+	  if (typeof objA !== 'object' || objA === null || typeof objB !== 'object' || objB === null) {
+	    return false;
+	  }
+
+	  var keysA = Object.keys(objA);
+	  var keysB = Object.keys(objB);
+
+	  if (keysA.length !== keysB.length) {
+	    return false;
+	  }
+
+	  // Test for A's keys different from B.
+	  var bHasOwnProperty = Object.prototype.hasOwnProperty.bind(objB);
+	  for (var i = 0; i < keysA.length; i++) {
+	    if (staticsMap[keysA[i]]) continue;
+	    if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+	      return false;
+	    }
+	  }
+
+	  return true;
+	}
+
+/***/ },
+/* 4 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_4__;
 
 /***/ }
 /******/ ])
